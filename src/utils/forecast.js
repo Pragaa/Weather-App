3966938786bded6a10d076522649cc946266f568
praga <gitgon@email.com>
2019-03-19 10:32:38 +0100

    solved destructuring bug

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,15 +4,15 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
   let weatherUrl = `https://api.darksky.net/forecast/f4485be64783bd69eb55f889e1c1bd01/${latitude},${longitude}?units=si`
 
-  request({url: weatherUrl, json: true}, (error, {body}) => {
+  request({url: weatherUrl, json: true}, (error, response) => {
     if (error) {
       callback('unable to connect to weather service!')
-    } else if (body.error) {
+    } else if (response.body.error) {
       callback('Unable to find location')
     } else {
       callback(undefined, {
-        temperature: body.currently.temperature,
-        precipitation: body.currently.precipProbability*100,
+        temperature: response.body.currently.temperature,
+        precipitation: response.body.currently.precipProbability*100,
       })
     }
   })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,18 +4,18 @@ const request = require('request');
 const geocode = (adress, callback) => {
   let geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${adress}.json?access_token=pk.eyJ1IjoicHJhZ2FhIiwiYSI6ImNqdDRpc2loODFrNzkzem14NjJndjNrcGkifQ._ZM8BhLrSdexRYPwdyD_QQ`
 
-  request( {url: geocodeUrl, json: true}, (error, {body}) => {
-    let {features: geo} = body;
+  request( {url: geocodeUrl, json: true}, (error, response) => {
+   
 
     if(error) {
       callback('unable to connect to location')
-    } else if (geo.length === 0) {
+    } else if (response.body.features.length === 0) {
       callback('unable to find location. Try another search')
     } else {
       callback(undefined, {
-        location: geo[0].place_name,
-        latitude: geo[0].center[1],
-        longitude: geo[0].center[0],
+        location: response.body.features[0].place_name,
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0],
       })
     }
   })
